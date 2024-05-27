50ba87b80a1c9b7c95ca773867b56f166c32b09e
Alexander Kyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2024-05-27 11:15:16 +1000

    Update main.tf

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -19,14 +19,11 @@ resource "azurerm_key_vault" "application" {
   name                = azurecaf_name.key_vault.result
   resource_group_name = var.resource_group
   location            = var.location
-
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 90
-
-  sku_name = "standard"
-
+  sku_name                   = "standard"
   tags = {
-    "environment"      = var.environment
+    "environment"     = var.environment
     "application-name" = var.application_name
   }
 }
@@ -35,7 +32,6 @@ resource "azurerm_key_vault_access_policy" "client" {
   key_vault_id = azurerm_key_vault.application.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
-
   secret_permissions = [
     "Set",
     "Get",
@@ -45,14 +41,28 @@ resource "azurerm_key_vault_access_policy" "client" {
   ]
 }
 
-resource "azurerm_key_vault_access_policy" "owners" {
+# Create Azure AD group for account owners
+resource "azuread_group" "account_owners" {
+  display_name     = "Account Owners"
+  security_enabled = true
+}
+
+# Assign Key Vault Reader role to the Account Owners group
+resource "azurerm_key_vault_access_policy" "account_owners_keyvault_access" {
   key_vault_id = azurerm_key_vault.application.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "${data.azurerm_client_config.current.tenant_id}/owners"
+  object_id    = azuread_group.account_owners.object_id
+
+  key_permissions = [
+    "get",
+  ]
 
   secret_permissions = [
-    "Get",
-    "List",
+    "get",
+  ]
+
+  certificate_permissions = [
+    "get",
   ]
 }
 
@@ -60,47 +70,40 @@ resource "azurerm_key_vault_secret" "database_username" {
   name         = "database-username"
   value        = var.database_username
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
 
 resource "azurerm_key_vault_secret" "database_password" {
   name         = "database-password"
   value        = var.database_password
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
 
 resource "azurerm_key_vault_secret" "auth0_secret" {
   name         = "auth0-secret"
   value        = var.auth0_secret
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
 
 resource "azurerm_key_vault_secret" "auth0_issuer_base_url" {
   name         = "auth0-issuer-base-url"
   value        = var.auth0_issuer_base_url
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
 
 resource "azurerm_key_vault_secret" "auth0_client_id" {
   name         = "auth0-client-id"
   value        = var.auth0_client_id
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
 
 resource "azurerm_key_vault_secret" "auth0_client_secret" {
   name         = "auth0-client-secret"
   value        = var.auth0_client_secret
   key_vault_id = azurerm_key_vault.application.id
-
-  depends_on = [ azurerm_key_vault_access_policy.client ]
+  depends_on   = [azurerm_key_vault_access_policy.client]
 }
-
