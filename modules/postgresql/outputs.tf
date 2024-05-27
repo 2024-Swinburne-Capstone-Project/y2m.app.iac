f165779ae33e22cb59e8ea8c125e3f940ff9b4f1
Alexander Kyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2024-05-27 12:16:16 +1000

    Update outputs.tf

--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -1,5 +1,5 @@
 output "database_url" {
-  value       = "postgres://${var.administrator_login}:${random_password.password.result}@${azurerm_postgresql_flexible_server.database.fqdn}:5432/${azurerm_postgresql_flexible_server_database.database.name}"
+  value       = "postgres://${var.administrator_login}:${random_password.password.result}@${azurerm_postgresql_flexible_server.database.fqdn}:5432/${azurerm_postgresql_flexible_server_database.database.name}?sslmode=require"
   description = "The PostgreSQL server URL with password."
   sensitive   = true
 }
